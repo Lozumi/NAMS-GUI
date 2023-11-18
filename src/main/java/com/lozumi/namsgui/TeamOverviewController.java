6b80f1ca271599f3b4bb30f78f0044caeec48742
Lozumi Everkine <fantasynico1026@gmail.com>
2023-11-18 23:06:46 +0800

    fix: Fix alarm function

--- src/main/java/com/lozumi/namsgui/TeamOverviewController.java
+++ src/main/java/com/lozumi/namsgui/TeamOverviewController.java
@@ -1,5 +1,6 @@
 package com.lozumi.namsgui;
 
+import javafx.scene.layout.StackPane;
 import com.lozumi.namsgui.model.Team;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -7,6 +8,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.stage.FileChooser;
 import javafx.util.StringConverter;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.scene.control.ScrollPane;
+
 
 import java.io.File;
 
@@ -33,6 +39,10 @@ public class TeamOverviewController {
     private Button resetButton;
     @FXML
     private ComboBox<Team> teamComboBox;
+    @FXML
+    private TextFlow guideTextFlow;
+    @FXML
+    private ScrollPane scrollPane;
 
     // 对主应用程序的引用
     private MainApp mainApp;
@@ -47,6 +57,12 @@ public class TeamOverviewController {
     // 用户选择的团队
     private Team chosenTeam;
 
+    @FXML
+    public void initialize() {
+        // 设置欢迎信息
+        guideTextFlow.getChildren().addAll(createColoredText("欢迎使用团队管理系统！本系统由Lozumi制作，当前版本1.0。\n请根据左侧边栏文字提示进行操作。",Color.GREEN));
+    }
+
     /**
      * 处理选择按钮点击事件。
      */
@@ -57,7 +73,7 @@ public class TeamOverviewController {
         chosenFile = fileChooser.showOpenDialog(mainApp.getPrimaryStage());
         if (chosenFile != null) {
             chosenTextArea.setText("文件路径：" + chosenFile.getAbsolutePath());
-            System.out.println("文件路径：" + chosenFile.getAbsolutePath());
+            appendSuccessText("选择成功！");
         }
     }
 
@@ -66,47 +82,39 @@ public class TeamOverviewController {
      */
     @FXML
     private void ReadButtonClicked() {
-        if (chosenFile != null) {
-            TeamParser teamParser = new TeamParser(chosenFile.getAbsolutePath());
-            teamList.addAll(teamParser.readTeamsFromFile());
+        try {
+            if (chosenFile != null && chosenTeam == null) {
+                TeamParser teamParser = new TeamParser(chosenFile.getAbsolutePath());
+                teamList.addAll(teamParser.readTeamsFromFile());
+
+                // 将观察列表数据添加到下拉框中
+                teamComboBox.setItems(teamList);
+
+                // 设置下拉框中每个选项的显示内容，只显示团队名
+                teamComboBox.setConverter(new StringConverter<Team>() {
+                    @Override
+                    public String toString(Team team) {
+                        return team == null ? null : team.getTeamName();
+                    }
+
+                    @Override
+                    public Team fromString(String string) {
+                        // 如果需要支持从字符串转为 Team 对象，可以在这里实现
+                        return null;
+                    }
+                });
+
+                // 默认展示第一个队伍
+                teamComboBox.setValue(teamList.get(0));
+                chosenTeam = teamList.get(0);
+                appendSuccessText("读取成功！");
+            } else {
+                appendErrorText("请勿重复读取！");
+            }
+        } catch (Exception e) {
+            appendErrorText("读取按钮点击时出现异常：" + e.getMessage());
+            e.printStackTrace();
         }
-
-        // 将观察列表数据添加到下拉框中
-        teamComboBox.setItems(teamList);
-
-        // 设置下拉框中每个选项的显示内容，只显示团队名
-//        teamComboBox.setCellFactory(new Callback<ListView<Team>, ListCell<Team>>() {
-//            @Override
-//            public ListCell<Team> call(ListView<Team> param) {
-//                return new ListCell<Team>() {
-//                    @Override
-//                    protected void updateItem(Team item, boolean empty) {
-//                        super.updateItem(item, empty);
-//                        if (item != null) {
-//                            setText(item.getTeamName());
-//                        } else {
-//                            setText(null);
-//                        }
-//                    }
-//                };
-//            }
-//        });
-        teamComboBox.setConverter(new StringConverter<Team>() {
-            @Override
-            public String toString(Team team) {
-                return team == null ? null : team.getTeamName();
-            }
-
-            @Override
-            public Team fromString(String string) {
-                // 如果需要支持从字符串转为 Team 对象，可以在这里实现
-                return null;
-            }
-        });
-
-        // 默认展示第一个队伍
-        teamComboBox.setValue(teamList.get(0));
-        chosenTeam = teamList.get(0);
     }
 
     /**
@@ -119,6 +127,7 @@ public class TeamOverviewController {
                 (observable, oldValue, newValue) -> {
                     showTeamDetails(newValue);
                     chosenTeam = newValue;
+                    //appendSuccessText("选择了"+newValue+"~");
                 });
     }
 
@@ -127,20 +136,27 @@ public class TeamOverviewController {
      */
     @FXML
     private void ParseButtonClicked() {
-        if (chosenFile == null) {
-            showAlert("请选择一个数据文件。");
-            return;
-        }
-
-        // 根据选定的格式（纯文本、XML、HTML）执行解析
-        if (plainTextRadioButton.isSelected()) {
-            resultTextArea.setText(PlainTextTeamFormatter.getSingletonInstance().formatTeam(chosenTeam));
-        } else if (xmlRadioButton.isSelected()) {
-            resultTextArea.setText(XMLTeamFormatter.getSingletonInstance().formatTeam(chosenTeam));
-        } else if (htmlRadioButton.isSelected()) {
-            resultTextArea.setText(HTMLTeamFormatter.getSingletonInstance().formatTeam(chosenTeam));
-        } else {
-            showAlert("请选择解析格式。");
+        try {
+            if (chosenFile != null) {
+                // 根据选定的格式（纯文本、XML、HTML）执行解析
+                if (plainTextRadioButton.isSelected()) {
+                    resultTextArea.setText(PlainTextTeamFormatter.getSingletonInstance().formatTeam(chosenTeam));
+                    appendSuccessText("成功解析为纯文字格式~");
+                } else if (xmlRadioButton.isSelected()) {
+                    resultTextArea.setText(XMLTeamFormatter.getSingletonInstance().formatTeam(chosenTeam));
+                    appendSuccessText("成功解析为XML格式~");
+                } else if (htmlRadioButton.isSelected()) {
+                    resultTextArea.setText(HTMLTeamFormatter.getSingletonInstance().formatTeam(chosenTeam));
+                    appendSuccessText("成功解析为HTML格式~");
+                } else {
+                    appendErrorText("请选择解析格式。");
+                }
+            } else {
+                appendErrorText("请首先读取团队。");
+            }
+        } catch (Exception e) {
+            appendErrorText("解析按钮点击时出现异常：" + e.getMessage());
+            e.printStackTrace();
         }
     }
 
@@ -149,20 +165,26 @@ public class TeamOverviewController {
      */
     @FXML
     private void resetButtonClicked() {
-        // 重置 UI 元素和相关对象
-        resultTextArea.clear();
-        plainTextRadioButton.setSelected(false);
-        xmlRadioButton.setSelected(false);
-        htmlRadioButton.setSelected(false);
+        try {
+            // 重置 UI 元素和相关对象
+            resultTextArea.clear();
+            plainTextRadioButton.setSelected(false);
+            xmlRadioButton.setSelected(false);
+            htmlRadioButton.setSelected(false);
 
-        // 清空已选择的文件
-        chosenFile = null;
-        chosenTextArea.clear();
+            // 清空已选择的文件
+            chosenFile = null;
+            chosenTextArea.clear();
 
-        // 清空已选择的团队和解析器对象
-        chosenTeam = null;
-        teamParser = null;
-        teamComboBox.getItems().clear();
+            // 清空已选择的团队和解析器对象
+            chosenTeam = null;
+            teamParser = null;
+            teamComboBox.getItems().clear();
+            appendSuccessText("重置完成！");
+        } catch (Exception e) {
+            appendErrorText("重置按钮点击时出现异常：" + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
 
@@ -173,6 +195,29 @@ public class TeamOverviewController {
         alert.setHeaderText(null);
         alert.setContentText(message);
         alert.showAndWait();
+    }
+
+    // 辅助方法，将异常信息添加到 guideTextArea 中，并设置为红色
+    private void appendErrorText(String error) {
+        Text errorText = new Text("\n" + error);
+        errorText.setFill(Color.RED);
+        guideTextFlow.getChildren().addAll(errorText);
+        scrollPane.setVvalue(scrollPane.getVmax());
+    }
+
+    // 辅助方法，将异常信息添加到 guideTextArea 中，并设置为红色
+    private void appendSuccessText(String success) {
+        Text successText = new Text("\n" + success);
+        successText.setFill(Color.GREEN);
+        guideTextFlow.getChildren().addAll(successText);
+        scrollPane.setVvalue(scrollPane.getVmax());
+    }
+
+    //创建颜色文字
+    private Text createColoredText(String text, Color color) {
+        Text coloredText = new Text(text);
+        coloredText.setFill(color);
+        return coloredText;
     }
 
     /**
